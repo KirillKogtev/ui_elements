24de36ebb3f55c01080ad43f0e40599794fdeada
Kirill Kogtev <kirillkogtev95@gmail.com>
2018-02-22 11:19:41 +0300

    add webpack config

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const styles = require('Button.css');
+const styles = require('./Button.css');
 
 class Button extends React.Component {
 
     render() {
+        console.log(styles);
         return <div className={styles.test}>12312</div>
     }
 
