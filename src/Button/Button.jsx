7e9356f293f892662640f0ac7c6b3a0f79d0e7ff
Kirill Kogtev <kirillkogtev95@gmail.com>
2018-02-22 10:45:55 +0300

    start create button

---
+++ src/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const styles = require('Button.css');
+
+class Button extends React.Component {
+
+    render() {
+        return <div className={styles.test}>12312</div>
+    }
+
+
+}
+
+export default Button;
