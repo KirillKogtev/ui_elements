17c835699734e988365b630834fea8023d4423ba
Kirill Kogtev <kirillkogtev95@gmail.com>
2018-02-27 10:38:58 +0300

    add dark style for input

--- src/stories/Input.jsx
+++ src/stories/Input.jsx
@@ -3,23 +3,47 @@ import Input from '../Input';
 import ThemeProvider from '../ThemeProvider';
 
 export default (
-    <ThemeProvider theme={'light_theme'}>
-        <div>
-            <div>
-                <Input placeholder={'Обычное состояние'}/>
+    <div>
+        <ThemeProvider theme={'light_theme'}>
+            <div style={{padding: '50px 0'}}>
+                <div>
+                    <Input placeholder={'Обычное состояние'}/>
+                </div>
+                <div>
+                    <Input autoFocus defaultValue={'Фокус'}/>
+                </div>
+                <div>
+                    <Input placeholder={'Введите что-нибудь'} defaultValue={'Заполненное поле'}/>
+                </div>
+                <div>
+                    <Input defaultValue={'Только просмотр'} readOnly/>
+                </div>
+                <div>
+                    <Input placeholder={'Не активное'} disabled/>
+                </div>
+                <div>
+                    <Input placeholder={'Пароль'} type={'password'}/>
+                </div>
             </div>
-            <div>
-                <Input autoFocus defaultValue={'Фокус'}/>
+        </ThemeProvider>
+        <ThemeProvider theme={'dark_theme'}>
+            <div style={{background: '#002d72', padding: '50px 0'}}>
+                <div>
+                    <Input placeholder={'Обычное состояние'}/>
+                </div>
+                <div>
+                    <Input autoFocus defaultValue={'Фокус'}/>
+                </div>
+                <div>
+                    <Input placeholder={'Введите что-нибудь'} defaultValue={'Заполненное поле'}/>
+                </div>
+                <div>
+                    <Input defaultValue={'Только просмотр'} readOnly/>
+                </div>
+                <div>
+                    <Input placeholder={'Не активное'} disabled={true}/>
+                </div>
             </div>
-            <div>
-                <Input placeholder={'Введите что-нибудь'} defaultValue={'Заполненное поле'}/>
-            </div>
-            <div>
-                <Input defaultValue={'Только просмотр'} readOnly/>
-            </div>
-            <div>
-                <Input placeholder={'Не активное'} disabled={true}/>
-            </div>
-        </div>
-    </ThemeProvider>
+        </ThemeProvider>
+    </div>
 )
